0d4a7c21d68476a6901cecfbf844c4caec19203b
code-lad <codelad1@gmial.com>
2020-08-17 10:44:50 +0200

    get started with vue

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+new Vue({
+  el: "#app",
+  data: {
+    title1: "Getting Started With Vue",
+    title2: "Data Rendering",
+    title3: "Data Binding",
+    title4: "Vue Events",
+    fname: "Wonderboy",
+    lname: "Sokhulu",
+    age: "25",
+    btnp: "btn btn-primary",
+    btnd: "btn btn-danger",
+    coords: [(x = 0), (y = 0)],
+  },
+  methods: {
+    changeAge(amount) {
+      this.age -= amount;
+    },
+    logEvent(event) {
+      console.log(event);
+    },
+  },
+});
