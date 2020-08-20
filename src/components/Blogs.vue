257c114b3d83248646b9de9b01810d7ff7a131b6
code-lad <codelad1@gmial.com>
2020-08-20 14:30:41 +0200

    vue working with Posts API

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="blogs">
-    <h2>{{title}}</h2>
+    <h2>{{ title }}</h2>
     <button @click="changeTitle">change title</button>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Blogs",
   data() {
     return {
       title: "Blogs",
+      posts: [],
     };
   },
   methods: {
@@ -19,16 +25,24 @@ export default {
     },
   },
   beforeCreate() {
-    alert("beforeCreate Blogs hook");
+    // alert("beforeCreate Blogs hook");
   },
   created() {
-    alert("created Blogs hook");
+    // alert("created Blogs hook");
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
   beforeUpdate() {
-    alert("beforeUpdate Blog hook");
+    // alert("beforeUpdate Blog hook");
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
