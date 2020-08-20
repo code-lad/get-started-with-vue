0a579649e93be1bd60e924f629e4e8a2e49cf35e
code-lad <codelad1@gmial.com>
2020-08-20 11:16:49 +0200

    dev with vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <div id="app__logo">
+      <img alt="Vue logo" src="./assets/logo.png" />
+    </div>
+    <Friends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar";
+import Friends from "./components/Friends";
+import OnlineFriends from "./components/OnlineFriends";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Friends,
+    OnlineFriends,
+  },
+  data() {
+    return {
+      friends: [
+        { name: "Wonderboy", online: true },
+        { name: "Ayanda", online: false },
+        { name: "Langelihle", online: true },
+        { name: "Mdu", online: true },
+        { name: "Lindo", online: false },
+      ],
+    };
+  },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload);
+      this.friends = this.friends.filter((friend) => {
+        return friend.name !== payload.name;
+      });
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +48,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app__logo {
+  text-align: center;
 }
 </style>
---
+++ src/components/Friends.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="myFriends">
+    <h2>My Friends</h2>
+    <div v-for="(friend, index) in friends" :key="index">
+      <span @click="unfriend(friend.name)">{{friend.name}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Friends",
+  props: ["friends"],
+  data() {
+    return {};
+  },
+  methods: {
+    unfriend(name) {
+      this.$emit("delete", { name });
+    },
+  },
+};
+</script>
+ 
+<style>
+</style>
